52a3309f98c337ceec24f8a3927b9dfe43273f37
Manuel Rodríguez <mrodriguez@whiteguru.com>
2022-08-26 08:31:45 +0200

    Definitions and web implementation

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,72 @@
+export interface AccessToken {
+  applicationId?: string;
+  declinedPermissions?: string[];
+  expires?: string;
+  isExpired?: boolean;
+  lastRefresh?: string;
+  permissions?: string[];
+  token: string;
+  userId?: string;
+}
+
+export interface AuthenticationToken {
+  token: string;
+  userId?: string;
+}
+
+export interface FacebookLoginResponse {
+  accessToken: AccessToken | null;
+  recentlyGrantedPermissions?: string[];
+  recentlyDeniedPermissions?: string[];
+}
+
+export interface FacebookLimitedLoginResponse {
+  authenticationToken: AuthenticationToken | null;
+}
+
+export interface FacebookCurrentAccessTokenResponse {
+  accessToken: AccessToken | null;
+}
+
 export interface FacebookLoginPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  initialize(options: Partial<FacebookConfiguration>): Promise<void>;
+  login(options: { permissions: string[] }): Promise<FacebookLoginResponse>;
+  loginWithLimitedTracking(options: {
+    permissions: string[];
+  }): Promise<FacebookLimitedLoginResponse>;
+  logout(): Promise<void>;
+  reauthorize(): Promise<FacebookLoginResponse>;
+  getCurrentAccessToken(): Promise<FacebookCurrentAccessTokenResponse>;
+  getProfile<T extends object>(options: {
+    fields: readonly string[];
+  }): Promise<T>;
+}
+
+export interface FacebookGetLoginStatusResponse {
+  status: 'connected';
+  authResponse: {
+    accessToken: string;
+    expiresIn: number;
+    reauthorize_required_in: number;
+    signedRequest: string;
+    userID: string;
+  };
+}
+
+export interface FacebookError {
+  message: string;
+  type: string;
+  code: number;
+}
+
+export interface FacebookGetProfileResponse {
+  error: FacebookError | null;
+}
+
+export interface FacebookConfiguration {
+  appId: string;
+  autoLogAppEvents: boolean;
+  xfbml: boolean;
+  version: string;
+  locale: string;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,204 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { FacebookLoginPlugin } from './definitions';
+import type {
+  FacebookLoginPlugin,
+  FacebookLoginResponse,
+  FacebookCurrentAccessTokenResponse,
+  FacebookGetLoginStatusResponse,
+  FacebookGetProfileResponse,
+  FacebookConfiguration,
+  FacebookLimitedLoginResponse,
+} from './definitions';
+
+declare interface Facebook {
+  init(
+    options: Partial<{
+      appId: string;
+      autoLogAppEvents: boolean;
+      xfbml: boolean;
+      version: string;
+    }>,
+  ): void;
+
+  login(handle: (response: any) => void, options?: { scope: string }): void;
+
+  loginWithLimitedTracking(
+    handle: (response: any) => void,
+    options?: { scope: string },
+  ): void;
+
+  logout(handle: (response: any) => void): void;
+
+  reauthorize(handle: (response: any) => void): void;
+
+  getLoginStatus(
+    handle: (response: FacebookGetLoginStatusResponse) => void,
+  ): void;
+
+  api<TResponse>(path: string, callback: (response: TResponse) => void): void;
+  api<TParams extends object, TResponse>(
+    path: string,
+    params: TParams,
+    callback: (response: TResponse) => void,
+  ): void;
+  api<TParams extends object, TResponse>(
+    path: string,
+    method: 'get' | 'post' | 'delete',
+    params: TParams,
+    callback: (response: TResponse) => void,
+  ): void;
+}
+
+declare var FB: Facebook;
+
+declare global {
+  interface Window {
+    fbAsyncInit: Function;
+  }
+}
 
 export class FacebookLoginWeb extends WebPlugin implements FacebookLoginPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  constructor() {
+    super({
+      name: 'FacebookLogin',
+      platforms: ['web'],
+    });
+  }
+
+  initialize(options: Partial<FacebookConfiguration>): Promise<void> {
+    const defaultOptions = { version: 'v10.0' };
+    return new Promise((resolve, reject) => {
+      try {
+        return this.loadScript(options.locale).then(() => {
+          FB.init({ ...defaultOptions, ...options });
+          resolve();
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  private loadScript(locale: string | undefined): Promise<void> {
+    if (typeof document === 'undefined') {
+      return Promise.resolve();
+    }
+    const scriptId = 'fb';
+    const scriptEl = document?.getElementById(scriptId);
+    if (scriptEl) {
+      return Promise.resolve();
+    }
+
+    const head = document.getElementsByTagName('head')[0];
+    const script = document.createElement('script');
+    return new Promise<void>(resolve => {
+      script.defer = true;
+      script.async = true;
+      script.id = scriptId;
+      script.onload = () => {
+        resolve();
+      };
+      script.src = `https://connect.facebook.net/${locale ?? 'en_US'}/sdk.js`;
+      head.appendChild(script);
+    });
+  }
+
+  async login(options: {
+    permissions: string[];
+  }): Promise<FacebookLoginResponse> {
+    console.log('FacebookLoginWeb.login', options);
+
+    return new Promise<FacebookLoginResponse>((resolve, reject) => {
+      FB.login(
+        response => {
+          console.debug('FB.login', response);
+
+          if (response.status === 'connected') {
+            resolve({
+              accessToken: {
+                token: response.authResponse.accessToken,
+              },
+            });
+          } else {
+            reject({
+              accessToken: {
+                token: null,
+              },
+            });
+          }
+        },
+        { scope: options.permissions.join(',') },
+      );
+    });
+  }
+
+  async loginWithLimitedTracking(_options: {
+    permissions: string[];
+  }): Promise<FacebookLimitedLoginResponse> {
+    throw new Error('Not available on Web');
+  }
+
+  async logout(): Promise<void> {
+    return new Promise<void>(resolve => {
+      FB.logout(() => resolve());
+    });
+  }
+
+  async reauthorize(): Promise<FacebookLoginResponse> {
+    return new Promise<FacebookLoginResponse>(resolve => {
+      FB.reauthorize(it => resolve(it));
+    });
+  }
+
+  async getCurrentAccessToken(): Promise<FacebookCurrentAccessTokenResponse> {
+    return new Promise<FacebookCurrentAccessTokenResponse>(
+      (resolve, reject) => {
+        FB.getLoginStatus(response => {
+          if (response.status === 'connected') {
+            const result: FacebookCurrentAccessTokenResponse = {
+              accessToken: {
+                applicationId: undefined,
+                declinedPermissions: [],
+                expires: undefined,
+                isExpired: undefined,
+                lastRefresh: undefined,
+                permissions: [],
+                token: response.authResponse.accessToken,
+                userId: response.authResponse.userID,
+              },
+            };
+            resolve(result);
+          } else {
+            reject({
+              accessToken: {
+                token: null,
+              },
+            });
+          }
+        });
+      },
+    );
+  }
+
+  async getProfile<T extends object>(options: {
+    fields: readonly string[];
+  }): Promise<T> {
+    const fields = options.fields.join(',');
+
+    return new Promise<T>((resolve, reject) => {
+      FB.api<{ fields: string }, FacebookGetProfileResponse>(
+        '/me',
+        { fields },
+        response => {
+          if (response.error) {
+            reject(response.error.message);
+
+            return;
+          }
+
+          resolve(<T>response);
+        },
+      );
+    });
   }
 }
